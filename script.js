dc8afe9a39bbb512c056cbac048d8c292096b5e5
subrata410 <subrata.rooy2022@gmail.com>
2022-12-29 12:27:59 +0600

    JavaScript Class Five

---
+++ script.js
@@ -0,0 +1,74 @@
+// JavaScript Ternary Operator
+// If else example
+// let age = 15;
+// if (age >= 18) {
+//     console.log("You are full age");
+// } else {
+//     console.log("You are not full age");
+// }
+
+// Ternary Operator
+// age >= 18 ? console.log("You are full age") : console.log("You are not full age");
+
+// Example two
+// let food;
+// if (age > 12) {
+//     food = "Chickan";
+// } else {
+//     food = "Ice Cream";
+// }
+// console.log(`He is like to eat ${food}`);
+
+//
+// console.log(`He is like to eat ${age > 12 ? "Chicken" : "Ice Cream"}`)
+
+// let age = 20;
+// console.log(`She is like to eat ${age > 18 ? "Barger" : "Biskit"}`)
+
+// 2. JavaScript Fanction
+// Example 1
+// function showText() {
+//     console.log("Some text");
+// }
+// showText();
+// showText();
+
+// // Example 2
+// function showName(name) {
+//     console.log(name);
+// }
+// showName("Adrita");
+// showName("Nitu");
+// showName("Subrata");
+
+// // Example 3
+// function calculateNumber(n1, n2) {
+//     console.log(n1 * n2);
+// }
+// calculateNumber(5, 3);
+// calculateNumber(10, 5);
+
+// Example 4
+// function averageMark(s1, s2, s3, s4, s5) {
+//     console.log((s1 + s2 + s3 + s4 + s5) / 5)
+// }
+// averageMark(88, 75, 92, 77, 86);
+
+// function averageMark(s1, s2, s3, s4, s5) {
+//     const average = (s1 + s2 + s3 + s4 + s5) / 5; return average;
+// }
+// console.log(averageMark(88, 75, 92, 77, 86));
+
+// A mark
+// const aAverageMark = averageMark(88, 75, 92, 77, 86);
+// console.log(aAverageMark);
+
+// // B mark
+// const bAvergeMark = averageMark(60, 79, 55, 34, 72);
+// console.log(bAvergeMark);
+
+// // C mark
+// const cAvergeMark = averageMark(50, 49, 55, 44, 62);
+// console.log(cAvergeMark);
+
+// console.log(aAverageMark, bAvergeMark, cAvergeMark);
